5dfddae7cd4a56109f0d7b9619463c6cdfe736d2
dungTam <dungtamtithoi88@gmail.com>
2024-05-26 00:52:13 +0700

    fix pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,5 @@
 <template>
-    <div v-motion :initial="{
-        opacity: 0,
-        y: 100,
-    }" :enter="{
-        opacity: 1,
-        y: 0,
-    }" :leave="{
-        y: -100,
-        opacity: 0,
-    }">
+    <div>
     </div>
 
 </template>
-<script>
-
-export default {
-    layout: "no-layout",
-
-}
-</script>
