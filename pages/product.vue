71b9c6047bf36698c9f02707cbf955c898ae563e
dungTam <dungtamtithoi88@gmail.com>
2024-05-26 02:13:35 +0700

    add svg in product

--- pages/product.vue
+++ pages/product.vue
@@ -84,18 +84,118 @@
                                     <button v-for="(item, index) in dataProduct" :key="index"
                                         :style="`background-color: ${item.color.code};`" @click="tabTo(index)"
                                         class="border-2 relative border-gray-300 me-4 rounded-full w-6 h-6">
-                                        <v-icon v-if="index == tabActive"
-                                            class="absolute -bottom-full -translate-y-1/4 left-2/4 -translate-x-2/4"
-                                            name="md-arrowdropup-twotone" color="black" scale="0.9"></v-icon>
-                                        <v-icon v-if="index == tabActive"
-                                            class="absolute -top-full rotate-180 translate-y-1/4 left-2/4 -translate-x-2/4"
-                                            name="md-arrowdropup-twotone" color="black" scale="0.9"></v-icon>
-                                        <v-icon v-if="index == tabActive"
-                                            class="absolute top-2/4 -translate-y-2/4 rotate-90 -left-2/4 -translate-x-1/4"
-                                            name="md-arrowdropup-twotone" color="black" scale="0.9"></v-icon>
-                                        <v-icon v-if="index == tabActive"
-                                            class="absolute  top-2/4 -translate-y-2/4 rotate-[270deg] -right-2/4 translate-x-1/4"
-                                            name="md-arrowdropup-twotone" color="black" scale="0.9"></v-icon>
+                                        <span v-if="index == tabActive"
+                                            class="absolute -bottom-3 -translate-y-full left-2/4 -translate-x-2/4"
+                                            name="md-arrowdropup-twotone" color="black" scale="0.9">
+                                            <svg width="5" height="5" viewBox="0 -4.5 20 20" version="1.1"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
+                                                    stroke-linejoin="round"></g>
+                                                <g id="SVGRepo_iconCarrier">
+                                                    <title>arrow_up [#337]</title>
+                                                    <desc>Created with Sketch.</desc>
+                                                    <defs> </defs>
+                                                    <g id="Page-1" stroke="none" stroke-width="1" fill="none"
+                                                        fill-rule="evenodd">
+                                                        <g id="Dribbble-Light-Preview"
+                                                            transform="translate(-260.000000, -6684.000000)"
+                                                            fill="#000000">
+                                                            <g id="icons" transform="translate(56.000000, 160.000000)">
+                                                                <path
+                                                                    d="M223.707692,6534.63378 L223.707692,6534.63378 C224.097436,6534.22888 224.097436,6533.57338 223.707692,6533.16951 L215.444127,6524.60657 C214.66364,6523.79781 213.397472,6523.79781 212.616986,6524.60657 L204.29246,6533.23165 C203.906714,6533.6324 203.901717,6534.27962 204.282467,6534.68555 C204.671211,6535.10081 205.31179,6535.10495 205.70653,6534.69695 L213.323521,6526.80297 C213.714264,6526.39807 214.346848,6526.39807 214.737591,6526.80297 L222.294621,6534.63378 C222.684365,6535.03868 223.317949,6535.03868 223.707692,6534.63378"
+                                                                    id="arrow_up-[#337]"> </path>
+                                                            </g>
+                                                        </g>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </span>
+                                        <span v-if="index == tabActive"
+                                            class="absolute -top-3 rotate-180 translate-y-full left-2/4 -translate-x-2/4"
+                                            name="md-arrowdropup-twotone" color="black" scale="0.9">
+                                            <svg width="5" height="5" viewBox="0 -4.5 20 20" version="1.1"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
+                                                    stroke-linejoin="round"></g>
+                                                <g id="SVGRepo_iconCarrier">
+                                                    <title>arrow_up [#337]</title>
+                                                    <desc>Created with Sketch.</desc>
+                                                    <defs> </defs>
+                                                    <g id="Page-1" stroke="none" stroke-width="1" fill="none"
+                                                        fill-rule="evenodd">
+                                                        <g id="Dribbble-Light-Preview"
+                                                            transform="translate(-260.000000, -6684.000000)"
+                                                            fill="#000000">
+                                                            <g id="icons" transform="translate(56.000000, 160.000000)">
+                                                                <path
+                                                                    d="M223.707692,6534.63378 L223.707692,6534.63378 C224.097436,6534.22888 224.097436,6533.57338 223.707692,6533.16951 L215.444127,6524.60657 C214.66364,6523.79781 213.397472,6523.79781 212.616986,6524.60657 L204.29246,6533.23165 C203.906714,6533.6324 203.901717,6534.27962 204.282467,6534.68555 C204.671211,6535.10081 205.31179,6535.10495 205.70653,6534.69695 L213.323521,6526.80297 C213.714264,6526.39807 214.346848,6526.39807 214.737591,6526.80297 L222.294621,6534.63378 C222.684365,6535.03868 223.317949,6535.03868 223.707692,6534.63378"
+                                                                    id="arrow_up-[#337]"> </path>
+                                                            </g>
+                                                        </g>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </span>
+                                        <span v-if="index == tabActive"
+                                            class="absolute top-2/4 -translate-y-2/4 rotate-90 -left-1/4 -translate-x-2/4"
+                                            name="md-arrowdropup-twotone" color="black" scale="0.9">
+                                            <svg width="5" height="5" viewBox="0 -4.5 20 20" version="1.1"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
+                                                    stroke-linejoin="round"></g>
+                                                <g id="SVGRepo_iconCarrier">
+                                                    <title>arrow_up [#337]</title>
+                                                    <desc>Created with Sketch.</desc>
+                                                    <defs> </defs>
+                                                    <g id="Page-1" stroke="none" stroke-width="1" fill="none"
+                                                        fill-rule="evenodd">
+                                                        <g id="Dribbble-Light-Preview"
+                                                            transform="translate(-260.000000, -6684.000000)"
+                                                            fill="#000000">
+                                                            <g id="icons" transform="translate(56.000000, 160.000000)">
+                                                                <path
+                                                                    d="M223.707692,6534.63378 L223.707692,6534.63378 C224.097436,6534.22888 224.097436,6533.57338 223.707692,6533.16951 L215.444127,6524.60657 C214.66364,6523.79781 213.397472,6523.79781 212.616986,6524.60657 L204.29246,6533.23165 C203.906714,6533.6324 203.901717,6534.27962 204.282467,6534.68555 C204.671211,6535.10081 205.31179,6535.10495 205.70653,6534.69695 L213.323521,6526.80297 C213.714264,6526.39807 214.346848,6526.39807 214.737591,6526.80297 L222.294621,6534.63378 C222.684365,6535.03868 223.317949,6535.03868 223.707692,6534.63378"
+                                                                    id="arrow_up-[#337]"> </path>
+                                                            </g>
+                                                        </g>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </span>
+                                        <span v-if="index == tabActive"
+                                            class="absolute  top-2/4 -translate-y-2/4 rotate-[270deg] -right-1/4 translate-x-2/4"
+                                            name="md-arrowdropup-twotone" color="black" scale="0.9">
+                                            <svg width="5" height="5" viewBox="0 -4.5 20 20" version="1.1"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
+                                                    stroke-linejoin="round"></g>
+                                                <g id="SVGRepo_iconCarrier">
+                                                    <title>arrow_up [#337]</title>
+                                                    <desc>Created with Sketch.</desc>
+                                                    <defs> </defs>
+                                                    <g id="Page-1" stroke="none" stroke-width="1" fill="none"
+                                                        fill-rule="evenodd">
+                                                        <g id="Dribbble-Light-Preview"
+                                                            transform="translate(-260.000000, -6684.000000)"
+                                                            fill="#000000">
+                                                            <g id="icons" transform="translate(56.000000, 160.000000)">
+                                                                <path
+                                                                    d="M223.707692,6534.63378 L223.707692,6534.63378 C224.097436,6534.22888 224.097436,6533.57338 223.707692,6533.16951 L215.444127,6524.60657 C214.66364,6523.79781 213.397472,6523.79781 212.616986,6524.60657 L204.29246,6533.23165 C203.906714,6533.6324 203.901717,6534.27962 204.282467,6534.68555 C204.671211,6535.10081 205.31179,6535.10495 205.70653,6534.69695 L213.323521,6526.80297 C213.714264,6526.39807 214.346848,6526.39807 214.737591,6526.80297 L222.294621,6534.63378 C222.684365,6535.03868 223.317949,6535.03868 223.707692,6534.63378"
+                                                                    id="arrow_up-[#337]"> </path>
+                                                            </g>
+                                                        </g>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </span>
                                     </button>
 
                                 </div>
@@ -105,8 +205,12 @@
                             </div>
                             <div class="flex justify-between">
                                 <span class="title-font font-medium italic text-2xl text-gray-900">
-                                    <p class="text-gray-500 text-base"><del>{{ Number(dataProduct[tabActive]?.price).toLocaleString() }}</del><sup>vnđ</sup></p>
-                                    <p class="text-lg text-gray-500">Chỉ còn: <span class="text-red-600 font-bold">{{ Number(dataProduct[tabActive]?.price).toLocaleString() }}<sup>vnđ</sup></span></p>
+                                    <p class="text-gray-500 text-base"><del>{{
+                                        Number(dataProduct[tabActive]?.price).toLocaleString()
+                                            }}</del><sup>vnđ</sup></p>
+                                    <p class="text-lg text-gray-500">Chỉ còn: <span class="text-red-600 font-bold">{{
+                                        Number(dataProduct[tabActive]?.price).toLocaleString()
+                                            }}<sup>vnđ</sup></span></p>
                                 </span>
                                 <p>
                                     <button
