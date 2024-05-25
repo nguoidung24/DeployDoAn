f5409ed889a6cf5d9417a8cefc8cfdecc1a8cf36
dungTam <dungtamtithoi88@gmail.com>
2024-05-26 01:38:46 +0700

    public

--- composables/useBaseURL.js
+++ composables/useBaseURL.js
@@ -1,11 +1,11 @@
 
 export const useBaseURL = async () => {
     const dataBaseURL = {
-        baseURL: "http://localhost:8008/api/",
-        // baseURL:"https://admin.thegioidilac.shop/api/",
+        // baseURL: "http://localhost:8008/api/",
+        baseURL:"https://admin.thegioidilac.shop/api/",
 
-        baseURLImage: "http://localhost:8008/"
-        // baseURLImage:"https://admin.thegioidilac.shop/api/",
+        // baseURLImage: "http://localhost:8008/",
+        baseURLImage:"https://admin.thegioidilac.shop/",
 
     }
     return useState('dataBaseURL', () => dataBaseURL)
