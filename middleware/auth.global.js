a59860be2f749e2ab3ea19bb5564ecb34f9b0f84
dungTam <dungtamtithoi88@gmail.com>
2024-05-26 12:36:29 +0700

    add middleware

---
+++ middleware/auth.global.js
@@ -0,0 +1,16 @@
+// middleware/auth.js
+import Cookies from "js-cookie";
+export default defineNuxtRouteMiddleware((to, from) => {
+    // const noAuth = ['login', 'index', 'products', 'product-id'];
+
+    // const getLoginInfo = Cookies.get('isLogin');
+    // const isLogin = Number(getLoginInfo)
+    
+    // if (isLogin && to.name == 'login')
+    //     return navigateTo('/');
+    // if (!noAuth.includes(to.name))
+    //     if (!isLogin)
+    //         return navigateTo('/login');
+    console.log(to,from);
+
+});
